2636bca1197c93c02470fe737f7f164edeffaab2
cyx <1142272771@qq.com>
2019-07-26 16:21:04 +0800

    增加外协采购收货

---
+++ src/views/AssInstore/setting.vue
@@ -0,0 +1,371 @@
+<!--  -->
+<template>
+  <div>
+    <my-header title="设置"></my-header>
+    <div class="z-top"></div>
+    <div class="title">收货设置</div>
+    <div class="wrapper">
+      <div>
+        <p>收货人员设置</p>
+        <div class="img-wrapper">
+          <div v-for="(item,index) in founderList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeReciveList','founderList')" class="tianjia"></div>
+        </div>
+      </div>
+      <div>
+        <p>收货通知设置</p>
+        <div class="img-wrapper">
+          <div v-for="(item,index) in founderManagerList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeFounderManagerList','founderManagerList')"  class="tianjia"></div>
+        </div>
+      </div>
+      <div>
+        <p>通知方式</p>
+        <div class="option-wrapper">
+         <div class="radio">
+           <cube-checkbox-group v-model="founderNotifyList" :options="notifyTypeList" :horizontal="true" shape="square"/>
+            <!-- <cube-radio-group v-model="setInfo.founderNotifyList['0'].code" :options="notifyTypeList" :horizontal="true"/> -->
+          </div>
+        </div>
+      </div>
+      <div class="switch-wrapper">
+        <p>是否显示金额</p>
+        <cube-switch v-model="founderIfMoney" ></cube-switch>
+      </div>
+    </div>
+    <div class="title">检验设置</div>
+    <div class="wrapper">
+      <div>
+        <p>检验人员设置</p>
+         <div class="img-wrapper">
+          <div v-for="(item,index) in verifyerList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeVerifyerList','verifyerList')"  class="tianjia"></div>
+        </div>
+      </div>
+           <div>
+        <p>质检通知设置</p>
+         <div class="img-wrapper">
+          <div v-for="(item,index) in verifyerManagerList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeVerifyerManagerList','verifyerManagerList')"  class="tianjia"></div>
+        </div>
+      </div>
+      <div>
+        <p>通知方式</p>
+        <div class="option-wrapper">
+             <div class="radio">
+               <cube-checkbox-group v-model="verifyerNotifyList" :options="notifyTypeList" :horizontal="true" shape="square"/>
+            <!-- <cube-radio-group v-model="setInfo.verifyerNotifyList['0'].code" :options="notifyTypeList" :horizontal="true"/> -->
+          </div>
+        </div>
+      </div>
+      <div class="switch-wrapper">
+        <p>是否显示金额</p>
+        <cube-switch v-model="verifyerIfMoney"></cube-switch>
+      </div>
+    </div>
+    <div class="title">特采设置</div>
+    <div class="wrapper">
+      <div>
+        <p>特采人员设置</p>
+           <div class="img-wrapper">
+          <div v-for="(item,index) in ratifyerList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeRatifyerList','ratifyerList')"  class="tianjia"></div>
+        </div>
+      </div>
+       <div>
+        <p>特采通知设置</p>
+           <div class="img-wrapper">
+          <div v-for="(item,index) in ratifyerManagerList" :key="index">
+            <img :src="item.avatar" alt>
+          </div>
+          <div @click="addTeam('changeRatifyerManagerList','ratifyerManagerList')"  class="tianjia"></div>
+        </div>
+      </div>
+      <div>
+        <p>通知方式</p>
+        <div class="option-wrapper">
+        <div class="radio">
+           <cube-checkbox-group v-model="ratifyerNotifyLisst" :options="notifyTypeList" :horizontal="true" shape="square"/>
+            <!-- <cube-radio-group v-model="setInfo.ratifyerNotifyLisst['0'].code" :options="notifyTypeList" :horizontal="true"/> -->
+          </div>
+        </div>
+      </div>
+      <div class="switch-wrapper">
+        <p>是否显示金额</p>
+        <cube-switch v-model="ratifyerIfMoney"></cube-switch>
+      </div>
+    </div>
+
+      <router-view></router-view>
+        <div class="zw"></div>
+      <div class="bot">
+        <div @click="save">保存设置</div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { setUpInfo, setUpUpdate, getNotifyType } from '@/api/instore/instore'
+import myHeader from '@/components/header'
+export default {
+  components: {
+    myHeader
+  },
+  data () {
+    return {
+      setInfo: {
+      //   verifyerNotifyList:[{}],
+      //    ratifyerNotifyLisst:[{}],
+      // founderNotifyList:[{}],
+      },
+      notifyTypeList: [],
+      founderNotifyList: [],
+      verifyerNotifyList: [],
+      ratifyerNotifyLisst: [],
+      founderIfMoney: false,
+      verifyerIfMoney: false,
+      ratifyerIfMoney: false
+    }
+  },
+  created () {
+    const that = this
+    this.getSetting()
+    getNotifyType().then(res => {
+      console.log(res.notifyTypeList)
+      const notifyTypeList = res.notifyTypeList.map(item => {
+        if (item.isUse) {
+          return { label: item.name, value: item.code }
+        }
+      })
+      that.notifyTypeList = notifyTypeList
+      console.log(notifyTypeList)
+    })
+  },
+  computed: {
+    founderList () {
+      return this.$store.state.founderList
+    },
+    founderManagerList () {
+      return this.$store.state.founderManagerList
+    },
+    verifyerManagerList () {
+      return this.$store.state.verifyerManagerList
+    },
+    verifyerList () {
+      return this.$store.state.verifyerList
+    },
+    ratifyerList () {
+      return this.$store.state.ratifyerList
+    },
+    ratifyerManagerList () {
+      return this.$store.state.ratifyerManagerList
+    }
+
+  },
+
+  methods: {
+    save () {
+      const verifyerNotifyList = this.verifyerNotifyList.map(item => {
+        return { code: item }
+      })
+      const founderNotifyList = this.founderNotifyList.map(item => {
+        return { code: item }
+      })
+      const ratifyerNotifyLisst = this.ratifyerNotifyLisst.map(item => {
+        return { code: item }
+      })
+      console.log(verifyerNotifyList,
+        founderNotifyList,
+        ratifyerNotifyLisst)
+      const newObject = Object.assign({}, this.setInfo, {
+        founderList: this.founderList,
+        founderManagerList: this.founderManagerList,
+        verifyerManagerList: this.verifyerManagerList,
+        verifyerNotifyList,
+        founderNotifyList,
+        ratifyerNotifyLisst,
+        founderIfMoney: this.founderIfMoney ? '1' : '0',
+        verifyerIfMoney: this.verifyerIfMoney ? '1' : '0',
+        ratifyerIfMoney: this.ratifyerIfMoney ? '1' : '0'
+      })
+      setUpUpdate(newObject).then(res => {
+        console.log(res)
+        this.showToast('保存成功')
+      }).catch(res => {
+        if (res.msg) {
+          this.showToast(res.msg)
+        }
+      })
+    },
+    addTeam (type, name) {
+      this.$router.push({
+        path: '/instore/setting/select',
+        query: {
+          type,
+          name
+        }
+      })
+    },
+    getSetting () {
+      setUpInfo().then(res => {
+        this.setInfo = res
+        this.founderNotifyList = res.founderNotifyList.map(item => item.code)
+        this.verifyerNotifyList = res.verifyerNotifyList.map(item => item.code)
+        this.ratifyerNotifyLisst = res.ratifyerNotifyLisst.map(item => item.code)
+        this.founderIfMoney = !!res.founderIfMoney
+        this.verifyerIfMoney = !!res.verifyerIfMoney
+        this.ratifyerIfMoney = !!res.ratifyerIfMoney
+        this.$store.commit('changeReciveList', res.founderList || [])
+        this.$store.commit('changeFounderManagerList', res.founderManagerList || [])
+        this.$store.commit('changeVerifyerManagerList', res.verifyerManagerList || [])
+        this.$store.commit('changeVerifyerList', res.verifyerList || [])
+        this.$store.commit('changeRatifyerList', res.ratifyerList || [])
+        this.$store.commit('changeRatifyerManagerList', res.ratifyerManagerList || [])
+      })
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+.z-top {
+  margin-top: 41px;
+}
+.title {
+  background: #f8f9fe;
+  color: #505050;
+  height: 30px;
+  line-height: 30px;
+  padding: 0 15px;
+  font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+  > div:nth-child(2) {
+    font-size: 12px;
+    color: #5495ff;
+  }
+}
+.title:first-child {
+  margin-top: 41px;
+}
+.wrapper {
+  padding: 0 30px 15px 30px;
+  > div {
+    > p {
+      font-size: 14px;
+      margin-top: 15px;
+    }
+    > div {
+      padding: 0 15px;
+      margin-top: 15px;
+    }
+
+    > div.img-wrapper {
+      margin-top:0;
+      > div {
+        float: left;
+        width: 50px;
+        height: 50px;
+        margin-right: 16px;
+        font-size: 52px;
+        margin-top:10px;
+        text-align:center;line-height:50px;
+        > img {
+          width: 50px;
+          height: 50px;
+        }
+      }
+    }
+    > div.img-wrapper::after {
+      content: "";
+      clear: both;
+      display: table;
+    }
+    > div.option-wrapper {
+      font-size: 14px;
+      display: flex;
+      justify-content: space-between;
+      > div {
+        font-size: 14px;
+        display: flex;flex-direction: wrap;
+        align-items: center;
+
+        > span {
+          width: 14px;
+          height: 14px;
+          border: 1px solid #303030;
+          border-radius: 50%;
+          margin-right: 15px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          position: relative;
+        }
+      }
+      > div.active {
+        > span {
+          border: 1px solid #5495ff;
+        }
+        > span::after {
+          content: "";
+          display: block;
+          width: 10px;
+          height: 10px;
+          background: #5495ff;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin-left: -5px;
+          margin-top: -5px;
+          border-radius: 50%;
+        }
+      }
+    }
+  }
+  > div.switch-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: 500;
+    color: rgba(48, 48, 48, 1);
+  }
+}
+.bot {
+  z-index: 8;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  background: #f8f9fe;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > div {
+    width: 80%;
+    height: 40px;
+    background: #5495ff;
+    color: #fff;
+    font-size: 16px;
+    line-height: 40px;
+    text-align: center;
+    border-radius: 6px;
+  }
+}
+.zw{
+  height:50px;
+}
+.tianjia{
+  background: url('../../../static/img/tianjia.png');
+  background-size:50px;
+}
+</style>
---
+++ src/views/AssInstore/use.vue
@@ -0,0 +1,275 @@
+<!--  -->
+<template>
+  <div class="wrapper">
+    <div class="bar">物料信息</div>
+    <div class="table">
+      <div>
+        <div>物料编码</div>
+        <div>{{info.matNo}}</div>
+      </div>
+      <div>
+        <div>物料描述</div>
+        <div>{{info.matDesc}}</div>
+      </div>
+      <div>
+        <div>物料类型</div>
+        <div>{{info.matTypeName}}</div>
+      </div>
+      <div>
+        <div>规格型号</div>
+        <div>{{info.matModel}}</div>
+      </div>
+
+      <div>
+        <div>订单数量</div>
+        <div>{{info.totalQuantity}}</div>
+      </div>
+      <div>
+        <div>已收数量</div>
+        <div>{{info.receivedQty}}</div>
+      </div>
+      <div>
+        <div>待检数量</div>
+        <div>{{info.waitQcQty}}</div>
+      </div>
+    </div>
+
+    <div class="bar">检验信息</div>
+    <div class="table">
+      <div>
+        <div>结论</div>
+        <div>
+          <cube-radio-group v-model="info.qcResult" :options="options3" :horizontal="true" />
+        </div>
+      </div>
+      <div>
+        <div>明细</div>
+        <div>
+          <div>合格:{{info.qualifiedQty}}</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <div>不合格:{{info.noQualifiedQty}}</div>
+        </div>
+      </div>
+      <div>
+        <div>检验模板</div>
+        <div></div>
+      </div>
+      <div>
+        <div>质检报告</div>
+        <div @click="zj">查看</div>
+      </div>
+      <div>
+        <div>问题描述</div>
+        <div>{{info.qcRemark||'无'}}</div>
+      </div>
+      <div>
+        <div>待采数量</div>
+        <div style="color:red">{{waitUse}}</div>
+      </div>
+    </div>
+    <div class="bar">处理信息</div>
+    <div class="table">
+      <div>
+        <div>处理</div>
+        <div>
+          <div class="flex-wrapper">
+            <div>
+              特采
+              <input type="number" v-model="specialQty" />
+            </div>
+            <div>
+              退货
+              <input type="number" v-model="returnQty" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>罚款</div>
+        <div>
+          <input type="number" placeholder="请填写" v-model="deduct" />
+        </div>
+      </div>
+      <div>
+        <div>备注</div>
+        <div>
+          <input type="text" placeholder="请填写" v-model="specialRemark" />
+        </div>
+      </div>
+    </div>
+    <div class="bor-bar"></div>
+    <div class="bot" @click="save">提交</div>
+  </div>
+</template>
+
+<script>
+import {
+  assInStoreInfo,
+  // purchUpdate,
+  assPurchSpecial
+  // purchQuality,
+  // getStoreHouse,
+  // getStoreRoom
+} from '@/api/assinstore/assinstore'
+export default {
+  data () {
+    return {
+      specialQty: '',
+      returnQty: '',
+      deduct: '',
+      specialRemark: '',
+      info: {},
+      waitUse: '',
+      options3: [
+        {
+          label: '合格',
+          value: '1',
+          disabled: true
+        },
+        {
+          label: '不合格(或未测完)',
+          value: '2',
+          disabled: true
+        }
+      ]
+    }
+  },
+  created () {
+    this.getInfo()
+  },
+
+  methods: {
+    zj () {
+      const list = this.wordList.map(item => {
+        return {
+          text: item.fileName,
+          value: item.fileUrl
+        }
+      })
+      this.picker = this.$createPicker({
+        title: '',
+        data: [list],
+        onSelect: this.selectHandle,
+        onCancel: () => {}
+      })
+      this.picker.show()
+    },
+    selectHandle (selectedVal, selectedIndex, selectedText) {
+      const url = selectedVal.join(', ')
+      window.open(url)
+    },
+    getInfo () {
+      const purchSubId = this.$route.query.purchSubId
+      console.log(purchSubId)
+      assInStoreInfo({ purchSubId }).then(res => {
+        console.log(res)
+        this.info = res.inStoreInfo
+        this.waitUse =
+          this.info.noQualifiedQty * 1 -
+          this.info.specialQty * 1 -
+          this.info.returnQty * 1
+        this.wordList = res.qualityList
+        this.pdfList = res.factoryReportList
+        this.storeHouseId = res.inStoreInfo.storeHouseId
+        this.storeRoomId = res.inStoreInfo.storeRoomId
+        console.log(this.storeHouseId, this.storeRoomId)
+        // if (this.storeHouseId) {
+        //   getStoreRoom({ storeHouseId: this.storeHouseId }).then(res => {
+        //     var romeList = res.storeRoomsConfList
+        //     var newRome = romeList.map((item, index) => {
+        //       return { text: item.storeRoomName, value: item.id }
+        //     })
+        //     this.storeRoomList = newRome
+        //   })
+        // }
+      })
+    },
+    save () {
+      if (!this.specialQty && !this.returnQty) {
+        this.showToast('请填写特采数和退货数')
+        return
+      }
+      if (!this.specialQty) {
+        this.specialQty = 0
+      }
+      if (!this.returnQty) {
+        this.returnQty = 0
+      }
+      if (this.specialQty * 1 + this.returnQty * 1 !== this.waitUse) {
+        this.showToast('特采与退货数量相加需要等于待采数量')
+        return
+      }
+
+      const purchSubId = this.$route.query.purchSubId
+      var data2 = Object.assign({}, this.info, {
+        purchSubId,
+        specialQty: this.specialQty,
+        returnQty: this.returnQty,
+        deduct: this.deduct,
+        specialRemark: this.specialRemark
+      })
+      assPurchSpecial(data2)
+        .then(res => {
+          this.getInfo()
+          this.showToast('修改成功')
+        })
+        .catch(err => {
+          if (err.msg) {
+            this.showToast(err.msg)
+          }
+        })
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+.wrapper {
+  padding: 15px;
+  .bar {
+    font-size: 16px;
+    margin-bottom: 15px;
+  }
+  .table {
+    margin-bottom: 15px;
+    > div {
+      > div:nth-child(2) {
+        text-align: right;
+        justify-content: flex-end;
+      }
+      > div {
+        display: felx;
+        align-items: center;
+        > input {
+          text-align: right;
+        }
+      }
+    }
+  }
+  .bot {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    line-height: 40px;
+    color: #fff;
+    background: #5496ff;
+    font-size: 14px;
+    text-align: center;
+  }
+  .bor-bar {
+    height: 40px;
+  }
+}
+.flex-wrapper {
+  display: flex;
+  justify-content: space-around;
+  flex: 1;
+  > div {
+    > input {
+      width: 40px;
+      border: 1px solid #999;
+      height: 20px;
+      line-height: 20px;
+    }
+  }
+}
+</style>
