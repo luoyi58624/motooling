ed416422bbb51be83476a447d1f9ffd7a6d35555
cyx <1142272771@qq.com>
2019-12-13 13:55:18 +0800

    二维码验证

--- src/views/scan/nav-receive.vue
+++ src/views/scan/nav-receive.vue
@@ -86,19 +86,21 @@ export default {
     },
     sao () {
       const vm = this
-      wx.scanQRCode({
-        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-        scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
-        success: function (res) {
-          alert(JSON.stringify(res) + '2')
-          var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
-          console.log(result)
-          vm.save(result)
-        },
-        fail: function (res) {
-          alert(JSON.stringify(res) + '1')
-        }
-      })
+      setTimeout(function () {
+        wx.scanQRCode({
+          needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+          scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
+          success: function (res) {
+            alert(JSON.stringify(res) + '2')
+            var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
+            console.log(result)
+            vm.save(result)
+          },
+          fail: function (res) {
+            alert(JSON.stringify(res) + '1')
+          }
+        })
+      }, 100)
     }
   },
   mounted () {
