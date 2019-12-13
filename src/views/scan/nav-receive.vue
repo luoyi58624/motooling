478a65940c8ffaf2b50c02aea0cb6998e3c180e5
cyx <1142272771@qq.com>
2019-12-13 09:31:47 +0800

    二维码验证

--- src/views/scan/nav-receive.vue
+++ src/views/scan/nav-receive.vue
@@ -82,7 +82,6 @@ export default {
       return config
     },
     sao () {
-      alert(123)
       wx.scanQRCode({
         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
         scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
