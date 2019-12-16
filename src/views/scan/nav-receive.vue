c98ded727dedbc9ae70406421b78fc2878553191
cyx <1142272771@qq.com>
2019-12-16 10:51:20 +0800

    jsdk

--- src/views/scan/nav-receive.vue
+++ src/views/scan/nav-receive.vue
@@ -6,7 +6,7 @@
 
     <div class="input-wrapper">
       <div @click="sao" style="margin-left:10px;">
-          <img src="../../assets/sao.png" alt />
+        <img src="../../assets/sao.png" alt />
       </div>
       <input type="text" placeholder="请输入单号" v-model="value" />
       <div @click="save(value)">
@@ -94,8 +94,7 @@ export default {
             console.log(result)
             vm.save(result)
           },
-          fail: function (res) {
-          }
+          fail: function (res) {}
         })
       }, 1000)
     }
@@ -104,12 +103,39 @@ export default {
     this.getwechat().then(config => {
       wx.config(config)
       wx.ready(function () {
+        // config信息验证成功后会执行ready方法,所有接口调用都必须在config接口获得结果之后
+        // config 是一个客户端的异步操作,所以如果需要在页面加载时调用相关接口,则须把相关接口放在ready函数中调用来确保正确执行.对于用户触发是才调用的接口,则可以直接调用,不需要放在ready函数中
         wx.checkJsApi({
-          jsApiList: ['scanQRCode'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+          // 判断当前客户端版本是否支持指定JS接口
+          jsApiList: ['scanQRCode'],
           success: function (res) {
-            alert(JSON.stringfy(res))
+            // 以键值对的形式返回，可用true，不可用false。如：{"checkResult":{"scanQRCode":true},"errMsg":"checkJsApi:ok"}
+            if (res.checkResult.scanQRCode === true) {
+              wx.scanQRCode({
+                // 微信扫一扫接口
+                desc: 'scanQRCode desc',
+                needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
+                success: function (res) {
+                  const getCode = res.resultStr // 当needResult 为 1 时，扫码返回的结果
+                  alert(getCode)
+                }
+              })
+            } else {
+              alert('抱歉，当前客户端版本不支持扫一扫')
+            }
+          },
+          fail: function (res) {
+            // 检测getNetworkType该功能失败时处理
+            alert('fail' + res)
           }
         })
+      })
+
+      /* 处理失败验证 */
+      wx.error(function (res) {
+        // config 信息验证失败会执行error函数,如签名过期导致验证失败,具体错误信息可以打开config的debug模式查看,也可以在返回的res参数中查看,对于SPA可以在这里更新签名
+        alert('配置验证失败: ' + res.errMsg)
       })
     })
   }
