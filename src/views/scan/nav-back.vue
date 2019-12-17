a897af4fdcf667696e702ef8f8a00c8f364194f2
cyx <1142272771@qq.com>
2019-12-17 13:27:59 +0800

    添加录音

--- src/views/scan/nav-back.vue
+++ src/views/scan/nav-back.vue
@@ -6,8 +6,11 @@
     </div>
 
     <div class="input-wrapper">
+      <div @click="sao" style="margin-left:10px;">
+        <img src="../../assets/sao.png" alt />
+      </div>
       <input type="text" placeholder="请输入单号" v-model="value" />
-      <div @click="save">
+      <div @click="save(value)">
         <img src="../../assets/arrow.png" alt />
       </div>
     </div>
@@ -16,6 +19,7 @@
 
 <script>
 import { getDeliveryAndReturn } from '@/api/instore/instore'
+import wx from 'weixin-js-sdk'
 export default {
   data () {
     return {
@@ -23,11 +27,12 @@ export default {
     }
   },
   methods: {
-    save () {
-      if (!this.value) {
+    save (value) {
+      if (value) {
         this.showToast('单号不能为空')
         return
       }
+      this.value = value
       getDeliveryAndReturn({ billNo: this.value, type: 0 })
         .then(res => {
           console.log(res)
@@ -65,11 +70,35 @@ export default {
             this.showToast(err.msg)
           }
         })
+    },
+    sao () {
+      const vm = this
+      wx.scanQRCode({
+        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
+        success: function (res) {
+          var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
+          console.log(result)
+          vm.save(result)
+        },
+        fail: function (res) {}
+      })
     }
+  },
+  mounted () {
+    this.getwechat().then(config => {
+      wx.config(config)
+      wx.ready(function () {})
+      /* 处理失败验证 */
+      wx.error(function (res) {
+        // config 信息验证失败会执行error函数,如签名过期导致验证失败,具体错误信息可以打开config的debug模式查看,也可以在返回的res参数中查看,对于SPA可以在这里更新签名
+        alert('配置验证失败: ' + res.errMsg)
+      })
+    })
   }
 }
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
 .img-wrapper {
   text-align: center;
   margin-top: 20px;
