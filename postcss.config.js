633564fc1e6c88af9c17ca35de3f5dafa4ad88e8
lidong <530555402@qq.com>
2020-11-09 09:44:14 +0800

    修改在协同中，选人的操作

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    'postcss-px2rem': {
-      remUnit: 37.5
-    }
+    autoprefixer: {}
+    // 'postcss-px2rem': {
+    //   remUnit: 37.5
+    // }
   }
 }
