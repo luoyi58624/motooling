a5707f07b4660395e851d65efc0288fb71e1d21e
liuguanfu <2656267916@qq.com>
2022-10-10 16:47:53 +0800

    vant新增全局组件Overlay、VanImage

--- src/vant.js
+++ src/vant.js
@@ -1,7 +1,9 @@
-import { Popup, Tab, Tabs, Search } from 'vant'
+import { Popup, Tab, Tabs, Search, Overlay, Image as VanImage } from 'vant'
 import Vue from 'vue'
 
 Vue.use(Popup)
 Vue.use(Tab)
 Vue.use(Tabs)
 Vue.use(Search)
+Vue.use(Overlay)
+Vue.use(VanImage)
